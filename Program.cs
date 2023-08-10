eb144de8704764b541451a39ee54acc249fa3f63
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-08-10 13:12:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Solution sol = new Solution();
+
+string[] strs = new string[]
+{
+    "()[]{}",
+    "{[]}",
+    "]"
+};
+foreach(var item in strs)
+{
+    sol.IsValid(item);
+}
---
+++ Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var chars = new Dictionary<char, char>()
+        {
+            {'(',')' },
+            {'{','}' },
+            {'[',']' }
+        };
+
+        Stack<char> stack = new Stack<char>(s.Length);
+        foreach (var current in s)
+        {
+            if (chars.ContainsKey(current))
+                stack.Push(current);
+            else
+            {
+                var lastKey = chars.First(x => x.Value == current).Key;
+                if (!stack.Any())
+                    return false;
+                if (stack.Pop() != lastKey)
+                    return false;
+            }
+        }
+        return (stack.Count == 0);
+    }
+}
